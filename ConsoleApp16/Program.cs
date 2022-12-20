8000f85573f797c2eae35a144e1591d8ce819057
Alitahmaz <alirt@code.edu.az>
2022-12-20 21:05:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("1 ci eded daxil edin:");
+           int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 ci eded daxil edin:");
+           int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a == 20 || b == 20 || (a + b == 20));
+        }
+    }
+}
